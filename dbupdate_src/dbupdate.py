f670b66eb42d115fdc9dcbf3683bbb049ca7dd02
BohMatej <87945844+BohMatej@users.noreply.github.com>
2023-08-17 22:35:15 +0200

    PC small dbupdate.py change

--- dbupdate_src/dbupdate.py
+++ dbupdate_src/dbupdate.py
@@ -4,50 +4,20 @@ import os
 
 DIRNAME = os.path.dirname(__file__)
 
-print("What do you want to do?")
-print("Enter 1 to update the database.")
-print("Enter 2 to synchronize aliases from aliases.csv with services.txt (LEGACY).")
-print("Enter 3 to synchronize aliases from stops.csv with services.txt and check for duplicate aliases.")
-action = input("Action: >")
+def main():
+    print("What do you want to do?")
+    print("Enter 1 to update the database.")
+    print("Enter 2 to synchronize aliases from aliases.csv with services.txt (LEGACY).")
+    print("Enter 3 to synchronize aliases from stops.csv with services.txt and check for duplicate aliases.")
+    action = input("Action: >")
 
-if (action == "2"):
-    print("Legacy command, use (3) instead.")
-    
+    if (action == "2"):
+        updateAliases()
+    if (action == "3"):
+        updateStops()
 
-'''
-    # read existing aliases.csv into variable
-    existing_aliases = set()
-    with open(os.path.join(DIRNAME, "data/aliases.csv"), "r") as aliases:
-        reader = csv.DictReader(aliases)
-        for row in reader:
-            existing_aliases.add(row["alias"])
 
-    # read services and select which aliases to add to aliases.csv
-    to_add_aliases = set()
-    with open(os.path.join(DIRNAME, "data/services.txt"), "r") as services:
-        for line in services:
-            line = line.strip()
-            if len(line) == 0:
-                continue
-            if line[0] in ["]", "}", "/"]:
-                continue
-            if line in existing_aliases:
-                continue
-            to_add_aliases.add(line)
-    
-    with open(os.path.join(DIRNAME, "data/aliases.csv"), "a", newline="") as aliases:
-        writer = csv.DictWriter(aliases, fieldnames=["alias", "truename"])
-        number_of_appends = 0
-        for item in to_add_aliases:
-            print(item)
-            writer.writerow({"alias": item, "truename": ""})
-            number_of_appends += 1
-        print(f"Append complete. Successfully added {number_of_appends} aliases. Now go correct them!")
-        aliases.close()
-'''
-
-if (action == "3"):
-
+def updateStops():
     # read existing stop aliases from stops.csv into variable
     existing_aliases = set()
     maximum_uid = 0
@@ -99,3 +69,40 @@ if (action == "3"):
         
         aliases.close()
 
+def updateAliases():
+    print("Legacy command, use (3) instead.")
+    
+    '''
+    # read existing aliases.csv into variable
+    existing_aliases = set()
+    with open(os.path.join(DIRNAME, "data/aliases.csv"), "r") as aliases:
+        reader = csv.DictReader(aliases)
+        for row in reader:
+            existing_aliases.add(row["alias"])
+
+    # read services and select which aliases to add to aliases.csv
+    to_add_aliases = set()
+    with open(os.path.join(DIRNAME, "data/services.txt"), "r") as services:
+        for line in services:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if line[0] in ["]", "}", "/"]:
+                continue
+            if line in existing_aliases:
+                continue
+            to_add_aliases.add(line)
+    
+    with open(os.path.join(DIRNAME, "data/aliases.csv"), "a", newline="") as aliases:
+        writer = csv.DictWriter(aliases, fieldnames=["alias", "truename"])
+        number_of_appends = 0
+        for item in to_add_aliases:
+            print(item)
+            writer.writerow({"alias": item, "truename": ""})
+            number_of_appends += 1
+        print(f"Append complete. Successfully added {number_of_appends} aliases. Now go correct them!")
+        aliases.close()
+    '''
+
+if __name__ == "__main__":
+    main()
