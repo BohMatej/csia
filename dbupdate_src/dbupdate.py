72e1689ef588166733e9d0efe7494111325042e3
BohMatej <87945844+BohMatej@users.noreply.github.com>
2023-08-17 22:23:25 +0200

    Aliases.csv replaced with stops.csv
    Also added a 3rd option for dbupdate.py

--- dbupdate_src/dbupdate.py
+++ dbupdate_src/dbupdate.py
@@ -6,12 +6,16 @@ DIRNAME = os.path.dirname(__file__)
 
 print("What do you want to do?")
 print("Enter 1 to update the database.")
-print("Enter 2 to synchronize aliases.csv with services.txt.")
+print("Enter 2 to synchronize aliases from aliases.csv with services.txt (LEGACY).")
+print("Enter 3 to synchronize aliases from stops.csv with services.txt and check for duplicate aliases.")
 action = input("Action: >")
 
 if (action == "2"):
-    # read existing aliases.csv into variable
+    print("Legacy command, use (3) instead.")
+    
 
+'''
+    # read existing aliases.csv into variable
     existing_aliases = set()
     with open(os.path.join(DIRNAME, "data/aliases.csv"), "r") as aliases:
         reader = csv.DictReader(aliases)
@@ -40,6 +44,58 @@ if (action == "2"):
             number_of_appends += 1
         print(f"Append complete. Successfully added {number_of_appends} aliases. Now go correct them!")
         aliases.close()
+'''
 
+if (action == "3"):
 
+    # read existing stop aliases from stops.csv into variable
+    existing_aliases = set()
+    maximum_uid = 0
+    duplicate_aliases = set()
+    with open(os.path.join(DIRNAME, "data/stops.csv"), "r", encoding="utf-8") as stops:
+        reader = csv.DictReader(stops)
+        for row in reader:
+            # check and warn for duplicate aliases
+            if row["alias"] in existing_aliases:
+                duplicate_aliases.add(row["alias"])
+            existing_aliases.add(row["alias"])
+            maximum_uid = int(row["uid"])
+
+    # read services and select which aliases to add to stops.csv
+    to_add_aliases = set()
+    with open(os.path.join(DIRNAME, "data/services.txt"), "r") as services:
+        for line in services:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if line[0] in ["]", "}", "/"]:
+                continue
+            if line in existing_aliases:
+                continue
+            to_add_aliases.add(line)
+    
+    # write new aliases into stops.csv
+    with open(os.path.join(DIRNAME, "data/stops.csv"), "a", newline="") as aliases:
+        writer = csv.DictWriter(aliases, fieldnames=["uid", "district", "truename", "alias"])
+        number_of_appends = 0
+        for item in to_add_aliases:
+            writer.writerow({"uid": maximum_uid+1, "district": "DISTRICT", "alias": item, "truename": "TRUENAME"})
+            number_of_appends += 1
+            maximum_uid += 1
+        
+        print("")
+        if number_of_appends == 0:
+            print("All aliases from services.txt are already present in stops.csv.")
+        else:
+            print(f"Sync complete. Successfully added {number_of_appends} aliases. Now go correct them!")
+
+        # warn user of duplicate aliases
+        if len(duplicate_aliases) == 0:
+            print("No duplicate aliases have been found.")
+        else:
+            print(f"{len(duplicate_aliases)} duplicate aliases found:")
+            for alias in duplicate_aliases:
+                print(alias)
+        
+        aliases.close()
 
