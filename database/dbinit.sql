1764c8103b36244a2611666409a74f28a90d5bcf
BohMatej <87945844+BohMatej@users.noreply.github.com>
2023-08-19 03:00:51 +0200

    PC Did bulk of work on updateAllDatabase()

--- database/dbinit.sql
+++ database/dbinit.sql
@@ -1,8 +1,44 @@
 BEGIN;
 CREATE TABLE IF NOT EXISTS stops (
-    stop_id INTEGER,
+    stop_id INTEGER PRIMARY KEY AUTOINCREMENT,
     district TEXT, 
     truename TEXT,
-    alias TEXT
+    alias TEXT UNIQUE
+);
+CREATE TABLE IF NOT EXISTS lines (
+    label TEXT UNIQUE NOT NULL,
+    looping_status INTEGER NOT NULL DEFAULT 0,
+    /* Looping status: 0 if not looping, 1 if tearlooping, 2 if looping */
+    color TEXT NOT NULL DEFAULT "9e9e9e"
+);
+CREATE TABLE IF NOT EXISTS services (
+    services_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    line_label TEXT NOT NULL,
+    stop_id INTEGER NOT NULL,
+    subservice INTEGER NOT NULL,
+    order_in_subservice INTEGER,
+    UNIQUE (line_label, subservice, order_in_subservice) ON CONFLICT REPLACE
+    FOREIGN KEY (line_label)
+        REFERENCES lines (label)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (stop_id)
+        REFERENCES stops (stop_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS nearstops (
+    nearstops_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    stopone_id INTEGER,
+    stoptwo_id INTEGER,
+    walktime INTEGER,
+    FOREIGN KEY (stopone_id)
+        REFERENCES stops (stop_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+    FOREIGN KEY (stoptwo_id)
+        REFERENCES stops (stop_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 );
 COMMIT;
--- database/dbupdate.py
+++ database/dbupdate.py
@@ -1,23 +1,197 @@
 import sqlite3
 import csv
 import os
+from itertools import chain
 
 DIRNAME = os.path.dirname(__file__)
 
 def main():
     print("What do you want to do?")
     print("Enter 1 to update the database.")
-    print("Enter 2 to synchronize aliases from aliases.csv with services.txt (LEGACY).")
-    print("Enter 3 to synchronize aliases from stops.csv with services.txt and check for duplicate aliases.")
+    print("Enter 2 to load all stop aliases from services.txt to the database.")
+    print("Enter 3 to synchronize aliases from stops.csv with services.txt and check for duplicate aliases (LEGACY).")
+    print("Enter 4 to synchronize aliases from aliases.csv with services.txt (NOT FUNCTIONAL).")
+    print("Enter 5 to load stops.csv into the database's stops table (LEGACY).")
+
     action = input("Action: >")
 
+    if (action == "1"):
+        updateAllDatabase()
     if (action == "2"):
-        updateAliases()
+        updateStopsDatabase()
     if (action == "3"):
-        updateStops()
+        updateStopsCSV()
+    if (action == "4"):
+        updateAliases()
+    if (action == "5"):
+        loadStops()
 
 
-def updateStops():
+def updateAllDatabase():
+
+    # calls updateStopsDatabase to establish relation between alias and stopID. District and truename are not necessary just yet.
+    updateStopsDatabase()
+
+    # establishes DB connection
+    conn = sqlite3.connect(os.path.join(DIRNAME, "mhdle.db"))
+    cur = conn.cursor()
+
+    # These lists each contain tuples of values, which will later be used in queries.
+    lines_tuples = []
+    colors_tuples = []
+    services_tuples = []
+
+    
+    # read services.txt
+    with open(os.path.join(DIRNAME, "data/services.txt"), "r") as services:
+        rownumber = 1
+        linenumber = None
+        subservicenumber = None
+        order_in_subservice = 0
+        color = None
+
+        # loop through rows in services.txt
+        for row in services:
+            row = row.strip().split(" ")
+
+            # ignore newlines in services.txt
+            if len(row[0]) == 0:
+                rownumber += 1
+                continue
+
+            # sort out tags
+            if row[0][0] == "/":
+                if row[0] == "/line":
+                    if subservicenumber != None or color != None:
+                        print(f"Error on line {rownumber} of services.txt. Subservice or color not 'None' for /line tag. Please fix.")
+                        return
+                    subservicenumber = None
+                    linenumber = row[1]
+                    #cur.execute("INSERT INTO lines (label) VALUES (?) ON CONFLICT DO NOTHING", (linenumber,))
+                    lines_tuples.append((linenumber,))
+                elif row[0] == "/subservice":
+                    if linenumber == None:
+                        print(f"Error on line {rownumber} of services.txt. Line number not set for /subservice tag. Please fix.")
+                        return
+                    try:
+                        temp = int(row[1])
+                    except ValueError:
+                        print(f"Error on line {rownumber} of services.txt. Subservice is not an integer. Please fix.")
+                        return
+                    order_in_subservice = 0
+                    if subservicenumber == None and temp != 1:
+                        print(f"Error on line {rownumber} of services.txt. Subservice does not begin at 1. Please fix.")
+                        return
+                    if subservicenumber != None:
+                        if temp != subservicenumber + 1:
+                            print(f"Error on line {rownumber} of services.txt. Subservice does not increase by 1. Please fix.")
+                            return
+                    subservicenumber = temp
+                elif row[0] == "/color":
+                    if linenumber == None:
+                        print(f"Error on line {rownumber} of services.txt. Line number not set for /color tag. Please fix.")
+                        return
+                    color = row[1]
+                    #cur.execute("UPDATE lines SET color = ? WHERE label = ?", (color, linenumber,))
+                    colors_tuples.append((color, linenumber,))
+            elif row[0][0] == "]":
+                if linenumber == None:
+                    print(f"Error on line {rownumber} of services.txt. Probably an extra bracket. Please fix.")
+                    return
+            elif row[0][0] == "}":
+                linenumber = None
+                color = None
+                subservicenumber = None
+            elif len(row) > 1:
+                print(f"Error on line {rownumber} of services.txt. Wrongly structured line. Please fix.")
+                return
+            
+            # sort out each subservice's stops
+            else:
+                stop_id = cur.execute("SELECT stop_id FROM stops WHERE alias = ?", (row[0],)).fetchone()[0]
+                services_tuples.append((linenumber, stop_id, subservicenumber, order_in_subservice,))
+                order_in_subservice += 1
+
+            rownumber += 1
+
+    # execute query on lines table
+    cur.executemany("INSERT OR REPLACE INTO lines (label) VALUES (?)", lines_tuples)
+
+    # execute query on lines table, adding line color
+    cur.executemany("UPDATE lines SET color = ? WHERE label = ?", colors_tuples)
+
+
+    # execute query on services table
+    cur.executemany("INSERT OR REPLACE INTO services (line_label, stop_id, subservice, order_in_subservice) VALUES (?, ?, ?, ?)", services_tuples)
+                
+    # execute query to remove unwanted values from lines table
+    mylist = []
+    for tuple in lines_tuples:
+        mylist.append(tuple[0])
+    cur.execute("DELETE FROM lines WHERE label NOT IN (%s)" % ','.join('?'*len(mylist)), mylist)
+
+    # execute query to remove unwanted values from services table
+    temp_line_label = []
+    temp_stop_id = []
+    temp_subservice = []
+    temp_order = []
+    for tuple in services_tuples:
+        temp_line_label.append(tuple[0])
+        temp_stop_id.append(tuple[1])
+        temp_subservice.append(tuple[2])
+        temp_order.append(tuple[3])
+    temp_master = chain(temp_line_label, temp_stop_id, temp_subservice, temp_order)
+    temp_master = (*temp_master, )
+    
+    mylist = cur.execute("""SELECT services_id FROM services 
+                         WHERE line_label IN (%s) 
+                         AND stop_id IN (%s) 
+                         AND subservice IN (%s) 
+                         AND order_in_subservice IN (%s)""" % (','.join('?'*len(services_tuples)), 
+                                                               ','.join('?'*len(services_tuples)), 
+                                                               ','.join('?'*len(services_tuples)), 
+                                                               ','.join('?'*len(services_tuples))),
+                                                               temp_master).fetchall()
+    myactualfuckinglist = []
+    for tuple in mylist:
+        myactualfuckinglist.append(tuple[0])
+    cur.execute("DELETE FROM services WHERE services_id NOT IN (%s)" % ','.join('?'*len(myactualfuckinglist)), myactualfuckinglist)
+
+    # commit queries
+    conn.commit()
+
+    # closes database
+    cur.close()
+    conn.close()
+
+
+def updateStopsDatabase():
+    conn = sqlite3.connect(os.path.join(DIRNAME, "mhdle.db"))
+    cur = conn.cursor()
+
+    # read services.txt and add stop aliases
+    list_of_tuples = []
+    with open(os.path.join(DIRNAME, "data/services.txt"), "r") as services:
+        for line in services:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if line[0] in ["]", "}", "/"]:
+                continue
+            list_of_tuples.append((line,))
+    
+    # execute DB query
+    cur.executemany("INSERT INTO stops (alias) VALUES (?) ON CONFLICT DO NOTHING", list_of_tuples)
+    conn.commit()
+        
+    cur.close()
+    conn.close()
+
+    print(f"Successfully added entries. Now go into the database and fix them!")
+
+
+def updateStopsCSV():
+
     # read existing stop aliases from stops.csv into variable
     existing_aliases = set()
     maximum_uid = 0
@@ -70,13 +244,14 @@ def updateStops():
     
     aliases.close()
 
+
 def updateAliases():
-    print("Legacy command, use (3) instead.")
+    print("Disfuncional command, use (3) instead.")
     
     '''
     # read existing aliases.csv into variable
     existing_aliases = set()
-    with open(os.path.join(DIRNAME, "data/aliases.csv"), "r") as aliases:
+    with open(os.path.join(DIRNAME, "data/legacy/aliases.csv"), "r") as aliases:
         reader = csv.DictReader(aliases)
         for row in reader:
             existing_aliases.add(row["alias"])
@@ -94,7 +269,7 @@ def updateAliases():
                 continue
             to_add_aliases.add(line)
     
-    with open(os.path.join(DIRNAME, "data/aliases.csv"), "a", newline="") as aliases:
+    with open(os.path.join(DIRNAME, "data/legacy/aliases.csv"), "a", newline="") as aliases:
         writer = csv.DictWriter(aliases, fieldnames=["alias", "truename"])
         number_of_appends = 0
         for item in to_add_aliases:
@@ -104,6 +279,22 @@ def updateAliases():
         print(f"Append complete. Successfully added {number_of_appends} aliases. Now go correct them!")
         aliases.close()
     '''
+
+
+def loadStops():
+    conn = sqlite3.connect(os.path.join(DIRNAME, "mhdle.db"))
+    cur = conn.cursor()
+
+    with open(os.path.join(DIRNAME, "data/stops.csv"), "r", encoding="utf-8") as stops:
+        reader = csv.DictReader(stops)
+        for row in reader:
+            # check and warn for duplicate aliases
+            cur.execute("INSERT INTO stops (stop_id, district, truename, alias) VALUES (?, ?, ?, ?) ON CONFLICT DO NOTHING;", (row["uid"], row["district"], row["truename"], row["alias"]))
+            conn.commit()
+
+    cur.close()
+    conn.close()
+
 
 if __name__ == "__main__":
     main()
