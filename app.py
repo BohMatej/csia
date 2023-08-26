c3bc0a30348916a697958d0beb76849256393354
BohMatej <87945844+BohMatej@users.noreply.github.com>
2023-08-26 23:13:20 +0200

    PC custom menu and line icons

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
-
-from flask import Flask, redirect, render_template, request, session, url_for
+import sys
+sys.path.append("mhdle_src")
+from flask import Flask, redirect, render_template, request, session, url_for, jsonify, make_response
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+import json
+from routegeneration import generateRoute, verifyRoute
 
 app = Flask(__name__)
 
@@ -14,12 +17,50 @@ Session(app)
 
 @app.route('/')
 def index():
-    return render_template("custommenu.html")
+    return redirect("/custom")
+
+@app.route('/custom', methods=["GET", "POST"])
+def custom():
+    if request.method == "POST":
+
+        if request.form.get("usegreylines") is None:
+            ugl = False
+        else:
+            ugl = True
+
+        
+
+        route = generateRoute(
+            length=int(request.form.get("btnroute")),
+            area=tuple(request.form.getlist("area")),
+            useGreyLines=ugl
+        )
+
+        route.printRoute()
+        return render_template("customgame.html")
+
+    else:
+        return render_template("custommenu.html")
 
 @app.route('/login')
 def login():
     return 'login'
 
+@app.route('/verifyroute', methods=["POST"])
+def verifyroute():
+    req = request.get_json()
+    print(req["lines"])
+    lines = tuple(req["lines"])
+
+    myroute = verifyRoute(lines) # buggy
+
+    if myroute is None:
+        res = {"message": "Valid"}
+    else:
+        res = {"message": "Invalid"}
+    
+    out = make_response(jsonify(res), 200)
+    return out
 
 with app.test_request_context():
     print("Hello")
