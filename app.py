72fb04aba434b915c77d60c504c01a07d46a960d
BohMatej <87945844+BohMatej@users.noreply.github.com>
2023-08-26 18:38:01 +0200

    Added custom game menu

--- app.py
+++ app.py
@@ -14,15 +14,12 @@ Session(app)
 
 @app.route('/')
 def index():
-    return 'index'
+    return render_template("custommenu.html")
 
 @app.route('/login')
 def login():
     return 'login'
 
-@app.route('/user/<username>')
-def profile(username):
-    return f'{username}\'s profile'
 
 with app.test_request_context():
     print("Hello")
